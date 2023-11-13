67fb32b46f5da1a2d846d619234ce50055e147db
Gerard lima <gerard321nuneslima123@gmail.com>
2023-11-13 05:06:22 -0300

    tela de cadastros e forms

--- app/app.py
+++ app/app.py
@@ -3,13 +3,17 @@ from flask import Blueprint, render_template
 
 app_bp = Blueprint('app', __name__)
 
-@app_bp.route('/login')
+@app_bp.route('/login', methods=['GET'])
 def index():
     return render_template('func.html')
 
-@app_bp.route('/m')
+@app_bp.route('/m', methods=['POST'])
 def menu():
     return render_template('adm.html')
+
+@app_bp.route('/card' )
+def cad():
+    return render_template('cad.html')
 
 
 if __name__=="__main__":
