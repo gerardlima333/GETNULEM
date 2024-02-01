ae3c51ebea4f460ef62e0b0f1c04d40e9efa7515
Gerard lima <gerard321nuneslima123@gmail.com>
2024-02-01 01:36:02 -0300

    adicionei card

--- get/app/models/__init__.py
+++ get/app/models/__init__.py
@@ -3,12 +3,18 @@ from app import db
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=False, nullable=False)
     senha = db.Column(db.String(250), unique=True, nullable=False)
-
+    card = db.relationship('Card', backref='perfil', lazy=True)
     def __repr__(self):
         return f'<Usuario {self.nome}>'
     
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    dieta = db.Column(db.String(300), nullable= True)
+    hora = db.Column(db.String(100), unique= False, nullable= False)    
+    livre = db.Column(db.String(100), unique= False, nullable= True)
+    perfil_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
 
 
 
--- get/app/views.py
+++ get/app/views.py
@@ -5,9 +5,12 @@ from app import db
 app_bp = Blueprint('app', __name__)
 
 
+@app_bp.route('/adm')   
+def mosti():
+    return render_template('adm.html')
 
+#criei uma rota para adicionar usuario simples.
 @app_bp.route('/adicionar_usuario', methods=['GET', 'POST'])
-
 def adicionar_usuario():
     if request.method == 'POST':
         nome = request.form['nome']
@@ -19,15 +22,16 @@ def adicionar_usuario():
         db.session.add(novo_usuario)
         db.session.commit()
         return render_template('adm.html')
-
-
     return redirect(url_for('app.index'))
 
 @app_bp.route('/enter', methods= ['GET', 'POST'])
+
 def rend():
      return render_template('log.html')
 
 @app_bp.route('/login', methods= ['GET','POST'])
+    
+
 def login():
     render_template('log.html')
     usuario = Usuario.query.all()
@@ -38,15 +42,12 @@ def login():
         if any(usus == u.nome and sens == u.senha for u in usuario):
             mensagem_erro2 = f'bem vindo {usus}'
     # Lógica para autenticação bem-sucedida
-            return render_template("adm.html", erro= mensagem_erro2)
+            return render_template("adm.html", erro= mensagem_erro2, nome = usus)
         else:
             mensagem_erro = "usuario nao encontrado."
             return render_template('log.html', erro = mensagem_erro)
-       
-    
+           
     return render_template('adm.html')
-
-  
 
 @app_bp.route('/', methods= ['GET'])
 def index():
@@ -70,24 +71,15 @@ def adicionar_usuario():
 @app_bp.route('/most', methods=['GET', 'POST'])
 def most():
     usuarios = Usuario.query.all()
-       
-
     return render_template('most.html', usuarios=usuarios)
 
 @app_bp.route('/delete/<int:usuario_id>', methods=['POST', 'GET'])
 def delete_usuario(usuario_id):
     usuario = Usuario.query.get(usuario_id)
-
+    
     if usuario:
         db.session.delete(usuario)
         db.session.commit()
 
     return redirect(url_for('app.most'))
         
-
-
-
-@app_bp.route('/adm')
-
-def mosti():
-    return render_template('adm.html')
